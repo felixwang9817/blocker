7c750270fd96f461b16e4d441c4e79cbabcf748b
Eric Nie <niee16@student.westboroughk12.org>
2018-12-23 22:20:51 -0500

    Initialize blocked sites

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ function addBlockedSite(site){
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+
+
     let startBlockingButton = document.getElementById('start-blocking');
     let stopBlockingButton = document.getElementById('stop-blocking');
-    let timer = document.getElementById('timer');
+    let timer = document.getElementById('timer-display');
     let seconds_remaining = null;
     var countdown = null;
 
@@ -33,7 +35,17 @@ document.addEventListener('DOMContentLoaded', () => {
             countdown = setInterval(update_timer, 1000);
             seconds_remaining = response.seconds_remaining;
         }
-    })
+    }); 
+
+    chrome.storage.local.get({'blocked_sites': ['www.youtube.com', 
+                                          'www.netflix.com', 
+                                          'www.reddit.com']}, 
+    (obj)=> {
+        let list = obj.blocked_sites; 
+        for(let i =0; i<list.length; i++){
+            addBlockedSite(list[i]); 
+        }
+    });
 
     startBlockingButton.addEventListener('click', function(){
         chrome.runtime.sendMessage({
